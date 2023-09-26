4c2a5da9db13e1d77c2b1ec97323a3e9f85617ed
Jhordan Sax <71660993@continental.edu.pe>
2023-09-26 15:05:14 -0500

    Mutador y Accesor - Semana 5

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+// Libreria para funcones con string
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Symfony\Component\HttpFoundation\RequestMatcher\AttributesRequestMatcher;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,4 +46,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // Agregando el metodo mutador para el campo nme del usuario
+    protected function name(): Attribute {
+        return new Attribute(
+            // Accesor
+            get: fn($value) => ucwords($value),
+            // Mutador
+            set: fn($value) => strtolower($value)
+        );
+    }
 }
